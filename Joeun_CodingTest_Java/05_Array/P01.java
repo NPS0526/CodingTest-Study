43b87b59ca17ee39113978ce677831d42ae143d2
Fine823 <worth3910@naver.com>
2026-07-01 14:26:31 +0900

    Solve skipped problems

---
+++ Joeun_CodingTest_Java/05_Array/P01.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class P01 {
+    public static int[] solution(int[] arr) {
+        Arrays.sort(arr);
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 3, 4, 1};
+        int[] result = solution(arr);
+        System.out.println(Arrays.toString(result));
+    }
+}
---
+++ Joeun_CodingTest_Java/05_Array/P02.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class P02 {
+    public static int[] solution(int[] arr) {
+        /* for 문 사용
+        arr = Arrays.stream(arr).distinct().sorted().toArray();
+        
+        for (int i=0; i<arr.length/2; i++){
+            int tmp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i] = tmp;
+        } */
+
+        // Collections.reverseOrder() 사용
+        arr = Arrays.stream(arr).distinct().boxed().sorted(Collections.reverseOrder()).mapToInt(Integer::intValue).toArray();
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 3, 4, 1};
+        int[] result = solution(arr);
+        System.out.println(Arrays.toString(result));
+    }
+}
