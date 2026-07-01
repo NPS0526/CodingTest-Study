a1b3dcc22c4775ee42fd165c1db6fd6551656eb7
rlojuyeon <ejy4632@naver.com>
2026-07-01 09:16:28 +0900

    [rloju] - 0701(짝지어 제거하기)

---
+++ rloju/Stack/P12973.java
@@ -0,0 +1,20 @@
+package Stack;
+
+import java.util.ArrayDeque;
+
+public class P12973 {
+    public int solution(String s){
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        
+        stack.push(s.charAt(0));
+        for(int i=1; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(stack.isEmpty() || c != stack.peek()) stack.push(c);
+            else{
+                stack.pop();
+            }
+        }
+        if(stack.isEmpty()) return 1;
+        else return 0;
+    }
+}
