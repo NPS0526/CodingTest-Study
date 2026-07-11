928595747847a7a95038b685d30c29c43a95a750
김창주 <micayell98@naver.com>
2026-07-12 04:56:17 +0900

    [changju] - 0712(프로세스)

---
+++ gukkaebi/07_Queue/P42587.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        ArrayDeque<Integer> prior_queue = new ArrayDeque<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(i);
+            prior_queue.add(priorities[i]);
+        }
+
+        int cnt = 0;
+        while (true) {
+            int curr_idx = queue.pop();
+            int curr_priority = prior_queue.pop();
+
+            boolean flag = true;
+            for (int priority : prior_queue) {
+                if (curr_priority < priority) {
+                    flag = false;
+                    queue.add(curr_idx);
+                    prior_queue.add(priority);
+                    break;
+                }
+            }
+
+            if (flag == ture)
+                cnt++;
+
+            if (flag == true && curr_idx == location) {
+                break;
+            }
+        }
+
+        return cnt;
+    }
+}
