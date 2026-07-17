46f6cb7a73da07e0905e5eb3ebd7de044f1ae819
김창주 <micayell98@naver.com>
2026-07-18 01:16:16 +0900

    [changju] - 0718(다단계 칫솔 판매)

---
+++ gukkaebi/08_Hash/22_P42579.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+        // int[] answer = {};
+        // return answer;
+        
+        
+    }
+}
