89a566c85c9edee53881e33cb6fa7b56c765a043
김창주 <micayell98@naver.com>
2026-07-22 17:23:37 +0900

    [changju] - 0722(양과늑대)

---
+++ gukkaebi/09_Tree/28_P92343.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+class Solution {
+    static List<Integer>[] graph;
+    static int[] gInfo;
+    static int maxSheep = 0;
+
+    static class State {
+        int node;
+        int sheep;
+        int wolf;
+        List<Integer> nextNodes;
+
+        public State(int node, int sheep, int wolf, List<Integer> nextNodes) {
+            this.node = node;
+            this.sheep = sheep;
+            this.wolf = wolf;
+            this.nextNodes = nextNodes;
+        }
+    }
+
+    public int solution(int[] info, int[][] edges) {
+        int n = info.length;
+        gInfo = info;
+        graph = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            int parent = edges[i][0];
+            int child = edges[i][1];
+            graph[parent].add(child);
+        }
+
+        bfs();
+
+        return maxSheep;
+    }
+
+    static void bfs() {
+        Queue<State> queue = new ArrayDeque<>();
+
+        List<Integer> startNextNodes = new ArrayList<>();
+        for (int child : graph[0]) {
+            startNextNodes.add(child);
+        }
+        
+        queue.offer(new State(0, 1, 0, startNextNodes));
+        maxSheep = 1;
+
+        while (!queue.isEmpty()) {
+            State current = queue.poll();
+
+            maxSheep = Math.max(maxSheep, current.sheep);
+
+            for (int next : current.nextNodes) {
+                int sheepCnt = current.sheep;
+                int wolfCnt = current.wolf;
+
+                if (gInfo[next] == 0) {
+                    sheepCnt++;
+                } else {
+                    wolfCnt++;
+                }
+
+                if (wolfCnt >= sheepCnt)
+                    continue;
+
+                List<Integer> nextAvailableNodes = new ArrayList<>(current.nextNodes);
+
+                nextAvailableNodes.remove(Integer.valueOf(next));
+
+                for (int child : graph[next]) {
+                    nextAvailableNodes.add(child);
+                }
+
+                queue.offer(new State(next, sheepCnt, wolfCnt, nextAvailableNodes));
+            }
+        }
+    }
+}
